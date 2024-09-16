2dde333db9a443139e776b4e82340a0691fe1827
subasuchi <subasuchi3119@gmail.com>
2024-09-17 02:16:40 +0800

    commit 1

--- src/com/example/Test.java
+++ src/com/example/Test.java
@@ -5,5 +5,10 @@ public class Test
 	public static void main(String[] args)
 	{
 		System.out.println("1");
+<<<<<<< Master
+=======
+		System.out.println("2");
+		System.out.println("3");
+>>>>>>> local
 	}
 }
